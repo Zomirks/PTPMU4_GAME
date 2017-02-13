f0f06763457e363a2c87169aa24178b9d6f934b3
Unknown <skrimoziidesign@gmail.com>
2017-02-13 10:37:18 +0100

    rajout css et délimitation terrain

--- js/personnage.js
+++ js/personnage.js
@@ -1,3 +1,13 @@
+var terra
+
+anim();
+
+function anim() {
+    .clearRect(0, 0, terrain.width, terrain.height);
+    .drawImage(joueur1.Sprite, joueur1.x, joueur1.y);
+    setTimeout(anim, 1000/60);
+}
+
 function personnage(img, x, y) {
     this.Sprite = new Image();
     this.Sprite.src = img;
@@ -7,4 +17,4 @@ function personnage(img, x, y) {
     this.previousY;
 }
 
-var joueur1 = new personnage("/img.mario.png",100,100);
+var joueur1 = new personnage("./img/mario.png", 100, 100);
