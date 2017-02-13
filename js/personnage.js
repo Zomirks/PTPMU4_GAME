203b7d5954d545081739ace3dbe4cd80fa3d6c38
Unknown <skrimoziidesign@gmail.com>
2017-02-13 12:00:36 +0100

    Ajout du code JS pour afficher et déplacer le personnage tout seul
    Affiche le personnage à partir d'un constructeur et grâce au CANVAS, ajout du code faire les animation du personnage à l'aide de sprite sheet et déplacement du personnage d'1px (glissement)

--- js/personnage.js
+++ js/personnage.js
@@ -1,20 +1,69 @@
-var terra
 
-anim();
+document.addEventListener("DOMContentLoaded", function(event) {  
+    
+    var gameCanvas = document.getElementById("graphic");
+    var graphx = gameCanvas.getContext('2d');
 
-function anim() {
-    .clearRect(0, 0, terrain.width, terrain.height);
-    .drawImage(joueur1.Sprite, joueur1.x, joueur1.y);
-    setTimeout(anim, 1000/60);
+//    var heroImg = new Image();
+//    heroImg.src = "./src/characters/hero_fix.png";
+    var joueur1 = new Personnage("./src/characters/hero_fix.png", 50, 50);
+    
+    function MainLoop() {
+//        Personnage fait une légère glissade lentement et sans arrêt 
+        joueur1.x += 1;
+        
+        graphx.clearRect(0, 0, gameCanvas.width, gameCanvas.height);
+        graphx.drawImage(joueur1.sprite, joueur1.x, joueur1.y);
+        requestAnimationFrame(MainLoop, 1000/60);
+    }
+
+    function Personnage(img, x, y) {
+        this.sprite = new Image();
+        this.sprite.src = img;
+        this.x = x;
+        this.y = y;
+    }
+    
+    MainLoop();
+});
+
+
+// Code pour faire des gif en sprite sheet
+var img_obj = {
+    'source': null,
+    'current': 0,
+    'total_frames': 16,
+    'width': 16,
+    'height': 16
+};
+
+var img = new Image();
+img.onload = function () { // Triggered when image has finished loading.
+    img_obj.source = img;  // we set the image source for our object.
+}
+img.src = 'img/filename.png'; // contains an image of size 256x16
+                              // with 16 frames of size 16x16
+
+function draw_anim(context, x, y, iobj) { // context is the canvas 2d context.
+    if (iobj.source != null)
+        context.drawImage(iobj.source, iobj.current * iobj.width, 0,
+                          iobj.width, iobj.height,
+                          x, y, iobj.width, iobj.height);
+    iobj.current = (iobj.current + 1) % iobj.total_frames;
+                   // incrementing the current frame and assuring animation loop
 }
 
-function personnage(img, x, y) {
-    this.Sprite = new Image();
-    this.Sprite.src = img;
-    this.x = x;
-    this.y = y;
-    this.previousX;
-    this.previousY;
+function on_body_load() { // <body onload='on_body_load()'>...
+    var canvas = document.getElementById('canvasElement');
+                 // <canvas id='canvasElement' width='320' height='200'/>
+    var context = canvas.getContext("2d");
+
+    setInterval((function (c, i) {
+                return function () {
+                    draw_anim(c, 10, 10, i);
+                };
+    })(context, img_obj), 100);
 }
 
-var joueur1 = new personnage("./img/mario.png", 100, 100);
+
+
