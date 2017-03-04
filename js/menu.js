bc22b4760f4419cada1b204b96d6501339e8f11f
Zomirks <skrimoziidesign@gmail.com>
2017-03-04 17:11:40 +0100

    Choisir Controles / Retour Menu quand mort
    J'ai rajouté la possibilité de choisir la façon de jouer que l'on souhaite : flèches ou ZQSD, avec ausi ajout d'éléments visuels pour permettre à l'utilisateur de savoir en quel mode il se trouve. Aussi j'ai rajouté sur le message de "GameOver" (deathScreen / deathMessage dans le code) un lien qui redirige vers index.html et permet de retourner au menu principal

--- js/menu.js
+++ js/menu.js
@@ -1,11 +1,14 @@
-document.addEventListener("DOMContentLoaded", function(event){
+document.addEventListener("DOMContentLoaded", function(event) {
     var menu = document.getElementById("menu");
+    var controler = document.getElementById("control");
     var newGame = document.getElementById("newGame");
     var scriptJeu = document.createElement("script");
     var myBody = document.getElementById("body");
-    scriptJeu.src = "./js/script.js"
-    newGame.addEventListener("click", function(){
+    scriptJeu.src = "./js/script.js";
+
+    newGame.addEventListener("click", function() {
         menu.style.display = "none";
+        controler.style.display = "flex";
         gameStart();
     });
 });
