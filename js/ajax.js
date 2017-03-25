f5570abae2578a93c024f152a878eef6693c40f6
Zomirks <skrimoziidesign@gmail.com>
2017-03-25 16:26:27 +0100

    Suppression Menu.js // Correction bug menu
    J'ai supprimé le fichier menu.js pour intégrer le code dans ajax.js, ce qui m'a permis de lancer une nouvelle partie après avoir regardé le classement des joueurs. Aussi j'ai ajouté un bouton retour pour que lorsque l'on regarde le classement, puisse retourner au menu pour commencer à jouer.

--- js/ajax.js
+++ js/ajax.js
@@ -1,3 +1,20 @@
+var menu = document.getElementById("menu");
+var controler = document.getElementById("control");
+var newGame = document.getElementById("newGame");
+var scriptJeu = document.createElement("script");
+var myBody = document.getElementById("body");
+scriptJeu.src = "./js/script.js";
+
+//		Lance une nouvelle partie
+document.addEventListener("DOMContentLoaded", function(event) {
+    newGame.addEventListener("click", function() {
+        menu.style.display = "none";
+        controler.style.display = "flex";
+        gameStart();
+    });
+});
+
+//		Affiche le classement des joueurs
 document.getElementById('ajax_button').addEventListener('click', function() {
     $.ajax({
         // URL du script coté serveur
@@ -15,33 +32,63 @@ document.getElementById('ajax_button').addEventListener('click', function() {
             $('#menu').append(ulClassement);
 
             var i = 1;
-
-            //id.appendChild(i);
-
-
-
             retour.score.forEach(function(article) {
                 var elt = document.createElement("li");
 
                 var id = document.createElement("div");
                 id.className = "classementPosition";
-				id.id = "position"+i;
-                id.append(i+'.');
-				elt.append(id);
+                id.id = "position" + i;
+                id.append(i + '.');
+                elt.append(id);
 
-				var name = document.createElement("div");
-				name.className = "classementName";
-				name.innerHTML = article.name;
-				elt.appendChild(name);
+                var name = document.createElement("div");
+                name.className = "classementName";
+                name.innerHTML = article.name;
+                elt.appendChild(name);
 
-				var score = document.createElement("div");
-				score.className = "classementScore";
-				score.innerHTML = article.score;
-				elt.appendChild(score);
+                var score = document.createElement("div");
+                score.className = "classementScore";
+                score.innerHTML = article.score;
+                elt.appendChild(score);
 
                 ulClassement.appendChild(elt);
-
                 i += 1;
+            });
+
+            var retourMenu = document.createElement("div");
+            retourMenu.id = "retourMenu";
+            retourMenu.innerHTML = "Retour";
+            $('#menu').append(retourMenu);
+
+			//			Permet de retourner au menu 
+            document.getElementById('retourMenu').addEventListener('click', function() {
+                $('#title').text("Game");
+                $('ul').remove();
+                retourMenu.remove();
+                var ulMenu = document.createElement("ul");
+                ulMenu.id = "ulMenu";
+                $('#menu').append(ulMenu);
+
+                var newGame = document.createElement("li");
+                newGame.id = "newGame";
+                newGame.innerHTML = "Nouvelle partie";
+                ulMenu.append(newGame);
+
+                var classement = document.createElement("li");
+                classement.id = "ajax_button";
+                classement.innerHTML = "Classement";
+                ulMenu.append(classement);
+
+                var credit = document.createElement("li");
+                credit.id = "credit";
+                credit.innerHTML = "Crédits";
+                ulMenu.append(credit);
+
+                newGame.addEventListener("click", function() {
+                    menu.style.display = "none";
+                    controler.style.display = "flex";
+                    gameStart();
+                });
             });
         }
     })
